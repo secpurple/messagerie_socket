f3bc45a0f8a89adb82f9053d21fdba595f196a2c
secpurple <102912527+secpurple@users.noreply.github.com>
2022-04-05 19:52:54 +0000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,38 @@
+#######################################
+## BY : https://github.com/secpurple ##
+## date : 05 april 2022              ##
+## sript : client                    ##
+#######################################
+
+import os
+import socket 
+from scapy.all import Ether, IP # pip install scappy
+
+def message():
+	message = input(" > : ")
+	send_message = ("""
+----------------------------------
+| envoyer par : """+hostname+""" 
+| addr_IPv4 / """+ip+"""      
+| addr_MAC / """+mac+"""      
+----------------------------------
+| => """+message)
+	client.send(send_message.encode('utf-8'))
+	if reponse == "fin":
+		print('le server a couppé la comunication ! ')
+		client.close()
+
+os.system('clear')
+hostname = socket.gethostname()
+host = 'localhost'
+port = 1024
+ip = IP(dst="0.0.0.0").src
+mac = Ether().src
+client = socket.socket()
+server = client.connect((host, port))
+print(server)
+client.send('bon'.encode('utf-8'))
+reponse = client.recv(255).decode("utf-8")
+print(reponse)
+
+message()
